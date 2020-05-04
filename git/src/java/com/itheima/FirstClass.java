ec3ecaeec347b0f622c225dc2ce48444d1953782
Mr.Zhang <finefinery@163.com>
2020-05-04 13:02:43 +0800

    git project is being commit second content: function add is created

--- git/src/java/com/itheima/FirstClass.java
+++ git/src/java/com/itheima/FirstClass.java
@@ -4,4 +4,10 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    private int add() {
+        int a = 4;
+        int b = a++;
+        return  a + b;
+    }
 }
