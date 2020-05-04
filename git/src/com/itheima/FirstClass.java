de035fecb3ba71404795de96aa53f05176fe1cab
Mr.Zhang <finefinery@163.com>
2020-05-04 13:36:05 +0800

    run and sout

--- git/src/com/itheima/FirstClass.java
+++ git/src/com/itheima/FirstClass.java
@@ -1,11 +1,13 @@
-package java.com.itheima;
+package com.itheima;
 
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
+
+        System.out.println(new FirstClass().add());
     }
 
-    private int add() {
+    public int add() {
         int a = 4;
         int b = a++;
         return  a + b;
